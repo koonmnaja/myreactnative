be9d09bb181fa1595317111d7fb07c5272cec5a2
koonmnaja <61019645+koonmnaja@users.noreply.github.com>
2022-01-27 14:15:31 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,36 +1,20 @@
-
-
-import { StyleSheet, Text, View } from 'react-native';
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import Ex3 from "./screens/week2/Ex3";
-import Ex2 from "./screens/week2/Ex2";
-
-import Welcome from "./screens/Welcome";
-import Ex1 from "./screens/week2/Challeng/Ex1";
-import Ex4 from "./screens/week2/Ex4";
-import Ex5 from "./screens/week2/Ex5";
-import Ex6 from "./screens/week2/Ex6";
-import Ex7 from "./screens/week2/Ex7";
-import Ex8 from "./screens/week2/Ex8";
-import Ex9 from "./screens/week2/Ex9";
-import Ex10 from "./screens/week2/Ex10";
-import Ex11 from "./screens/week2/Ex11";
-import Ex12 from "./screens/week2/Ex12";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import HomeStack from './navigations/HomeStack';
+import BottomTab from './navigations/BottomTab';
+import RootStack from './navigations/RootStack';
 
 
 export default function App() {
- 
-  return(
-     <Ex12/>
-    );
-}
+  return (
+    <NavigationContainer>
+            {/* <HomeStack /> */}
+            {/* <BottomTab /> */}
+            <RootStack />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+        </NavigationContainer>
+
+
+  );
+}
