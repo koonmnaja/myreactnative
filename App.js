fb14afea25d6f9dbf353b93d76cc07478ad3470e
koonmnaja <61019645+koonmnaja@users.noreply.github.com>
2022-04-02 13:50:28 +0700

    now

--- App.js
+++ App.js
@@ -4,16 +4,17 @@ import { NavigationContainer } from '@react-navigation/native';
 import HomeStack from './navigations/HomeStack';
 import BottomTab from './navigations/BottomTab';
 import RootStack from './navigations/RootStack';
+import { AuthContext, AuthContextProvider } from "./hooks/AuthContextProvider";
 
 
 export default function App() {
   return (
+    <AuthContextProvider>
     <NavigationContainer>
-            {/* <HomeStack /> */}
-            {/* <BottomTab /> */}
-            <RootStack />
+      <RootStack />
+    </NavigationContainer>
+  </AuthContextProvider>
 
-        </NavigationContainer>
 
 
   );
